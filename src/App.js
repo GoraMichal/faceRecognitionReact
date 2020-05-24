2ae10a784cdc52446e64eef474194f94859b6a63
Michał Góra <michalgorazs3@gmail.com>
2020-05-24 22:28:58 +0200

    particles

--- src/App.js
+++ src/App.js
@@ -5,11 +5,26 @@ import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLink from './components/ImageLink/ImageLink';
 import Rank from './components/Rank/Rank';
+import Particles from 'react-particles-js';
+
+const particlesOptions = {
+    particles: {
+        line_linked: {
+            shadow: {
+                enable: true,
+                color: "#3CA9D1",
+                blur: 5
+            }
+        }
+
+    }
+}
 
 function App() {
     return (
         <div className="App">
             <header>
+                <Particles className="particles" params={particlesOptions} />
                 <Navigation />
                 <Logo />
                 <Rank />
